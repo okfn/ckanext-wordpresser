47f3bdecf54f044e4d79668100477cda633d0b7f
Seb Bacon <seb.bacon@gmail.com>
2011-02-10 10:26:43 +0000

    comment source; handle 404s and 401s better

--- ckanext/wordpresser/middleware.py
+++ ckanext/wordpresser/middleware.py
@@ -1,4 +1,5 @@
 import paste.proxy
+from paste.deploy.converters import asbool
 from lxml.html import tostring, fromstring
 from ckan.lib.base import render
 from webob import Request
@@ -24,6 +25,7 @@ class WordpresserMiddleware(object):
         req.remove_conditional_headers(remove_encoding=True)
         follow = True
         proxy_url = proxy_host
+        path_info = environ['PATH_INFO']
         while follow:
             # deal with redirects internal to Wordpress
             wp_resp = req.get_response(paste.proxy.Proxy(proxy_url))
@@ -31,7 +33,7 @@ class WordpresserMiddleware(object):
                      and proxy_host in wp_resp.location
             environ['PATH_INFO'] = ''
             proxy_url = wp_resp.location
-
+        environ['PATH_INFO'] = path_info
         if wp_resp.status_int == 500:
             pass  # validation errors etc
         if wp_resp.status_int in [301, 302]:
@@ -45,12 +47,14 @@ class WordpresserMiddleware(object):
         # now try getting CKAN page
         req = Request(environ)
         ckan_resp = req.get_response(self.app)
-        if ckan_resp.status_int in [301, 302]:
+        if ckan_resp.status_int in [301, 302, 401]:
             return ckan_resp(environ, start_response)
-        if ckan_resp.status_int == 404:
-            if wp_resp.status_int == 404:
-                abort(404)
-            # CKAN 404s are ugly; we'll use a blank template instead
+        ckan_error = None
+        if ckan_resp.status_int >= 400:
+            # we don't necessarily get an attractive 404 page due to
+            # the order of the middleware, so we grab the index
+            # template instead and put the error in that
+            ckan_error = fromstring(ckan_resp.body).xpath('//body')
             pylons_app = PylonsApp()
             pylons_app.setup_app_env(environ, start_response)
             registry = environ['paste.registry']
@@ -60,15 +64,23 @@ class WordpresserMiddleware(object):
         else:
             # We're going to use the content we got from CKAN
             ckan_etree = fromstring(ckan_resp.body)
+
         # append WP nav onto CKAN nav
         wp_nav = wp_etree.xpath('//div[@class="menu"]/ul/*')
         ckan_etree_nav = ckan_etree.xpath('//div[@class="menu"]/ul')
         if ckan_etree_nav:
             ckan_etree_nav[0].extend(wp_nav)
-        if ckan_resp.status_int == 404:
-            # interpolate wordpress content into ckan content area
-            wp_error = wp_etree.xpath('//body[@id="error-page"]')
-            if wp_error:
+
+        # insert WP content into CKAN content area, if required
+        if ckan_resp.status_int >= 400:
+            if wp_resp.status_int == 404:
+                # Return our local 'not found' error
+                proxy_content = ckan_error[0]
+                proxy_content.tag = "div"
+                proxy_content.attrib['id'] = "content"
+            elif wp_resp.status_int >= 400:
+                # return Wordpress error
+                wp_error = wp_etree.xpath('//body[@id="error-page"]')
                 # set Wordpress' error text to be wrapped in our content div
                 proxy_content = wp_error[0]
                 proxy_content.tag = "div"
@@ -81,6 +93,7 @@ class WordpresserMiddleware(object):
             ckan_etree_content_container = ckan_etree_content.getparent()
             ckan_etree_content_container.remove(ckan_etree_content)
             ckan_etree_content_container.append(proxy_content)
+
         output = tostring(ckan_etree)
         output = output.replace(proxy_host, "/")
         start_response("200 OK", [('Content-Type', 'text/html'),
