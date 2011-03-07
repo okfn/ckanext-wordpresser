c6d09fb865944c5443815e4c92091e92a94e2d64
Seb Bacon <seb.bacon@gmail.com>
2011-03-07 15:28:18 +0000

    update setuptools metadata

--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ setup(
 	version=version,
 	description="Extend CKAN content with content from Wordpress",
 	long_description="""\
+        This CKAN extension allows you to overlay the standard CKAN
+	navigation with extra content proxied from a Wordpress
+	installation, bringing CMS capabilities to your CKAN install.
+	You can also use it to create a blog page, enable comments, etc.
 	""",
 	classifiers=["Development Status :: 2 - Pre-Alpha",
                      "Framework :: Pylons"], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='wordpress wsgi middleware ckan',
 	author='Seb Bacon',
-	author_email='seb.bacon@gmail.com',
+	author_email='seb.bacon@okfn.org',
 	url='',
 	license='GPL',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
